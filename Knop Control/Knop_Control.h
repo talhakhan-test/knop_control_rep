188bf45211513a17220ab4b3a45a33988171d0b1
Irfan Ali <irfan@whizpool.com>
2020-02-26 14:41:16 +0500

    Initial Commit

---
+++ Knop Control/Knop_Control.h
@@ -0,0 +1,19 @@
+//
+//  Knop_Control.h
+//  Knop Control
+//
+//  Created by Talha Khan on 26/02/2020.
+//  Copyright © 2020 Talha Khan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Knop_Control.
+FOUNDATION_EXPORT double Knop_ControlVersionNumber;
+
+//! Project version string for Knop_Control.
+FOUNDATION_EXPORT const unsigned char Knop_ControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Knop_Control/PublicHeader.h>
+
+
